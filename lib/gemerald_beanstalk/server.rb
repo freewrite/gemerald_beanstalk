4820845e953ed273b3f5aa4789bc046889e286b6
Danny Guinther <dannyguinther@gmail.com>
2013-12-24 06:38:48 -0500

    Add ability to stop server by address and wait for servers to avoid race conditions

--- lib/gemerald_beanstalk/server.rb
+++ lib/gemerald_beanstalk/server.rb
@@ -2,19 +2,60 @@ require 'eventmachine'
 
 module GemeraldBeanstalk::Server
 
-  def self.start(bind_address = nil, port = nil)
+  @@servers = ThreadSafe::Cache.new
+
+
+  def self.address_components(bind_address = nil, port = nil)
     bind_address ||= '0.0.0.0'
-    port ||= 11300
-    full_address = "#{bind_address}:#{port}"
-    beanstalk = GemeraldBeanstalk::Beanstalk.new(full_address)
-    thread = Thread.new do
+    port = port.nil? ? 11300 : Integer(port)
+    return {
+      :bind_address => bind_address,
+      :full_address => "#{bind_address}:#{port}",
+      :port => port,
+    }
+  end
+
+
+  def self.start(bind_address = nil, port = nil)
+    components = address_components(bind_address, port)
+    raise RuntimeError, "Server already exists for address #{components[:full_address]}" if @@servers.key?(components[:full_address])
+    beanstalk = GemeraldBeanstalk::Beanstalk.new(components[:full_address])
+    server_thread = Thread.new do
       EventMachine.run do
-        EventMachine.start_server(bind_address, port, GemeraldBeanstalk::Server, beanstalk)
+        EventMachine.start_server(components[:bind_address], components[:port], GemeraldBeanstalk::Server, beanstalk)
         EventMachine.add_periodic_timer(0.01, beanstalk.method(:update_state))
       end
     end
-    $PROGRAM_NAME = "gemerald_beanstalk:#{full_address}"
-    return [thread, beanstalk]
+    $PROGRAM_NAME = "gemerald_beanstalk:#{components[:full_address]}"
+    thread_and_beanstalk = [server_thread, beanstalk]
+    wait_for_server(:start, components[:bind_address], components[:port])
+    @@servers[components[:full_address]] = thread_and_beanstalk
+    return thread_and_beanstalk
+  end
+
+
+  def self.stop(bind_address = nil, port = nil)
+    components = address_components(bind_address, port)
+    serv = @@servers.delete(components[:full_address])
+    raise "No server found with address #{components[:full_address]}" if serv.nil?
+    serv[0].kill
+    wait_for_server(:stop, components[:bind_address], components[:port])
+  end
+
+
+  def self.wait_for_server(action, bind_address, port)
+    action = action.to_sym
+    loop do
+      begin
+        TCPSocket.new(bind_address, port)
+      rescue Errno::ECONNREFUSED
+        next if action == :start
+        break if action == :stop
+      rescue Errno::ECONNRESET
+        break if action == :stop
+      end
+      break if action == :start
+    end
   end
 
 
